1e6fcf73d804a08ce35e0e8af0fc197fefe54f9c
Richard C Gerkin <rick1@gerkin.org>
2022-10-21 13:44:30 -0700

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-export PATH=$PATH:$HOME/anaconda3_420/bin
-conda install -y -c rdkit -c mordred-descriptor mordred
-wget https://github.com/dream-olfaction/olfaction-prediction/archive/thin.zip
-unzip thin.zip
-mv olfaction-prediction-thin olfaction-prediction
